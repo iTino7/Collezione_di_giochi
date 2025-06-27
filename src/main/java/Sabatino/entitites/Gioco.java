7027292d2b349c89b2805f53d89b3771baee97ad
iTino7 <Sabatino.b007@outlook.it>
2025-06-27 11:27:00 +0200

    Initial commit

---
+++ src/main/java/Sabatino/entitites/Gioco.java
@@ -0,0 +1,51 @@
+package Sabatino.entitites;
+
+public abstract class Gioco {
+    private long id;
+    private String title;
+    private int year;
+    private double price;
+
+    public Gioco(long id, String title, int year, double price) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) throw new IllegalArgumentException("Il prezzo deve essere positivo");
+
+        this.price = price;
+    }
+
+    public abstract void stampaInfo();
+
+    @Override
+    public String toString() {
+        return "Gioco{" +
+                "id=" + id +
+                ", titolo='" + title + '\'' +
+                ", anno= " + year +
+                ", prezzo= " + price +
+                "}";
+    }
+
+
+}
